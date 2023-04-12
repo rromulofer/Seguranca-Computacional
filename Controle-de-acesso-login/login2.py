6e2cab2330fbf273cb1fdb31c8989ae3be8d7810
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-04-12 00:38:51 -0300

    update

--- Controle-de-acesso-login/login2.py
+++ Controle-de-acesso-login/login2.py
@@ -11,11 +11,10 @@
 # 3 - Token de acesso
 
 # Bibliotecas utilizadas
-
 # Gera o token de acesso de forma aleatória
 import random 
 
-# Utilizada junto com a random para gerar tokens aleatórios
+# Utilizada junto com a random para gerar senhas aleatórias
 # Também para verificar se a senha possui caracteres especiais
 import string
 
@@ -120,7 +119,7 @@ access_token_label = tk.Label(root, text="Token de acesso:")
 access_token_entry = tk.Entry(root)
 submit_button = tk.Button(root, text="Entrar", command=lambda: two_factor_auth(username_entry.get(), password_entry, access_token_entry, message_label, strength_label))
 verify_token_button = tk.Button(root, text="Verificar token", command=verify_access_token)
-message_label = tk.Label(root, text="Digite seu nome de usuário e senha")
+message_label = tk.Label(root, text="Digite seu nome de usuário")
 strength_label = tk.Label(root, text="Força da senha:")
 
 # Posiciona os widgets na janela
